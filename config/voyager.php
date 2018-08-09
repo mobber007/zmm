4474fb81b5798fafee31806f8e7a137bdc751035
mobber007 <thefactsmob@gmail.com>
2018-08-09 18:44:34 +0300

    Super commit

--- config/voyager.php
+++ config/voyager.php
@@ -15,7 +15,7 @@ return [
         'default_role'                 => 'user',
         'namespace'                    => null,
         'default_avatar'               => 'users/default.png',
-        'redirect'                     => '/admin',
+        'redirect'                     => '/',
     ],
 
     /*
@@ -201,7 +201,7 @@ return [
     |
     */
 
-    'primary_color' => '#22A7F0',
+    'primary_color' => '#411f6b',
 
     'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
 
